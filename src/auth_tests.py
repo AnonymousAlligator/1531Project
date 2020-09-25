def9eccdce6391792cfb7814a0d6d7496d0d8694
Taimoor Shabih <z5206259@ad.unsw.edu.au>
2020-09-25 20:27:30 +1000

    almost done with auth tests

--- src/auth_tests.py
+++ src/auth_tests.py
@@ -31,3 +31,60 @@ def test_auth_login():
         auth_login("", "pword14602")
         auth_login("registered@email.com", "12345")
         auth_login("invalid.com", "a12345678")
+
+########################################################
+
+def test_auth_logout():
+
+    deactivate("validregistered@email.com")
+    auth_dictionary = auth_register("validregistered@email.com", "1password", "Sam", "Drake")
+    user_token = auth_dictionary["token"]
+    invalid_token = "invalidtoken" # don't know if I need this
+
+    # Token is invalid token
+    assert auth_logout(invalid_token) == {'is_success': False} # don't know if I need this
+
+    # When the token is valid 
+    assert auth_logout(user_token) == {'is_success': True}
+
+    #When token is already logged out
+    assert auth_logout(user_token) == {'is_success': False}
+
+
+def test_auth_register():
+
+    # When an email has a valid format but not registered yet
+    auth_register("registered@valid.com", "potato321", "Elena", "Fisher")
+
+    # When the email has a valid format but is already registered
+    with pytest.raises(InputError):
+        auth_register("registered@valid.com", "potato321", "Elena", "Fisher")
+
+    # When the email is of an invalid format 
+    with pytest.raises(InputError):
+        auth_register("email.com", "jamaica45678", "Elena", "Drake")
+
+    with pytest.raises(InputError):
+        auth_register("invalid@email", "fortunehunter", "Victor", "Sullivan")
+    
+    with pytest.raises(InputError):
+        auth_register("two@@email.com", "narnia5566", "Nadine", "Ross")
+
+    with pytest.raises(InputError):
+        auth_register("eeeemail", "iamsotiredloool", "Nadine", "Ross")
+
+    # When the first name is too long
+    with pytest.raises(InputError):
+        auth_register("sample@email.com", "harrypotter", "toomanychars"*20, "Vic")
+
+    # When the first name is too long
+    with pytest.raises(InputError):
+        auth_register("sample@email.com", "okaygonnasleepnow", "toomanychars", "Vic"*30)
+
+    # When the password isn't valid
+    with pytest.raises(InputError):
+        auth_register("sample@email.com", "smol", "firstName", "lastName")
+    
+
+
+
