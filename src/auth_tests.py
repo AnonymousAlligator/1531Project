a069ab62821970976e1f61caa1f944008f32a36c
Taimoor Shabih <z5206259@ad.unsw.edu.au>
2020-09-25 19:48:07 +1000

    auth login tests done??

--- src/auth_tests.py
+++ src/auth_tests.py
@@ -4,10 +4,30 @@ import pytest
 def test_auth_login():
 
 # Initial function calls before testing
-    deactivate("registeredemail@valid.com")
+    
     auth_dictionary = auth_register("registeredemail@valid.com", "password1", "Nathan", "Drake")
     user_token = auth_dictionary["token"]
-    auth_logout(user_token)
+    auth_logout(user_token) #logout after registering
     
-# When the email is registered already with valid syntax
+    # When the email is registered already with valid syntax
     auth_login("registeredemail@valid.com", "password1") == {"token" : user_token}
+
+    # When the email is already logged in
+    with pytest.raises(InputError):
+        auth_login("registeredemail@valid.com", "password1")
+    auth_logout(user_token)
+
+    # When the email is valid but password is not correct
+    with pytest.raises(InputError):
+        auth_login("registeredemail@valid.com", "invalidpassword1")
+    
+
+    # When the email has valid syntax but is not registered (doesn't belong to a user)
+    with pytest.raises(InputError):
+        auth_login("unregisteredemail@valid.com", "password3")
+    
+    # When the input parameters are of invalid type or length
+    with pytest.raises(InputError):
+        auth_login("", "pword14602")
+        auth_login("registered@email.com", "12345")
+        auth_login("invalid.com", "a12345678")
