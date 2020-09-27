17acdaf15c2392651af1c3b84e0a1613b39f45a9
Kevin Hu <kevinhu12@hotmail.com>
2020-09-27 23:25:56 +1000

    Updated the tests files I wrote to reflect our assumptions

--- src/channel_details_test.py
+++ src/channel_details_test.py
@@ -1,4 +1,4 @@
-from channel import channel_details, channel_invite, channel_addowner
+from channel import channel_details, channel_invite
 from channels import channels_create
 from auth import auth_register
 from other import clear
@@ -6,35 +6,30 @@ import error
 import pytest
 
 # Setting up all the variables needed for test to run ############################################
+clear()
 Benjamin = auth_register("Benjamin@email.com", "password", "Benjamin", "Long")  # ID = 0
 Ross = auth_register("Ross@email.com", "password", "Ross", "Short")             # ID = 1
 Alex = auth_register("Alex@email.com", "password", "Alex", "Smith")             # ID = 2
 
-channels_create(Benjamin['token'], "Channel0", True)  # ID = 0
-channels_create(Ross['token'], "Channel1", False)     # ID = 1
+channel_id0 = channels_create(Benjamin['token'], "Channel0", True)  # ID = 0
+channel_id1 = channels_create(Ross['token'], "Channel1", False)     # ID = 1
 
 # Everyone is in channel 0, Benjamin is owner
-channel_invite(Benjamin['token'], 0, 0)
-channel_invite(Ross['token'], 0, 1)
-channel_invite(Alex['token'], 0, 2)
-channel_addowner(Benjamin['token'], 0, 0)
+channel_invite(Ross['token'], channel_id0, Ross['u_id'])
+channel_invite(Alex['token'], channel_id0, Alex['u_id'])
 
 # Ross and Alex are in channel 1, Ross is owner
-channel_invite(Ross['token'], 1, 1)
-channel_invite(Alex['token'], 1, 2)
-channel_addowner(Ross['token'], 1, 1)
+channel_invite(Alex['token'], channel_id1, Alex['u_id'])
 ##################################################################################################
 
 def test_channel_details_public():
-    clear()
-    details = channel_details(Benjamin['token'], 0)
+    details = channel_details(Benjamin['token'], channel_id0)
     assert details['name'] == 'Channel0'
     assert details['owner_members'] == 'Benjamin Long'
     assert details['all_members'] == ['Benjamin Long', 'Ross Short', 'Alex Smith']
 
 def test_channel_details_private():
-    clear()
-    details = channel_details(Ross['token'], 1)
+    details = channel_details(Ross['token'], channel_id1)
     assert details['name'] == 'Channel1'
     assert details['owner_members'] == 'Ross Short'
     assert details['all_members'] == ['Ross Short', 'Alex Smith']
@@ -43,16 +38,12 @@ def test_channel_details_invalid_channel():
     clear()
     #The channel doesn't exist
     #This should throw InputError
-    token = Benjamin['token']
-    channel_id = 2
     with pytest.raises(error.InputError):
-        assert channel_details(token, channel_id)
+        assert channel_details(Benjamin['token'], 2)
 
 def test_channel_details_not_a_member():
     clear()
     #User not a member of the channel
     #This should throw AccessError
-    token = Benjamin['token']
-    channel_id = 1
     with pytest.raises(error.AccessError):
-        assert channel_details(token, channel_id)
+        assert channel_details(Benjamin['token'], channel_id1)
--- src/channel_join_test.py
+++ src/channel_join_test.py
@@ -1,44 +1,35 @@
 from channel import channel_join, channel_invite
 from channels import channels_create
+from auth import auth_register
 from other import clear
 import error
 import pytest
-from auth import auth_register
 
 # Setting up all the variables needed for test to run ############################################
+clear()
 Benjamin = auth_register("Benjamin@email.com", "password", "Benjamin", "Long")  # ID = 0
 Ross = auth_register("Ross@email.com", "password", "Ross", "Short")             # ID = 1
 Alex = auth_register("Alex@email.com", "password", "Alex", "Smith")             # ID = 2
 
 # Channel1 is a private channel
-channels_create(Benjamin['token'], "Channel0", True)  # ID = 0
-channels_create(Ross['token'], "Channel1", False)     # ID = 1
+channel_id0 = channels_create(Benjamin['token'], "Channel0", True)  # ID = 0
+channel_id1 = channels_create(Ross['token'], "Channel1", False)     # ID = 1
 
 # Ross and Alex are in channel 1 but not Benjamin
-channel_invite(Ross['token'], 1, 1)
-channel_invite(Alex['token'], 1, 2)
+channel_invite(Alex['token'], channel_id1, Alex['u_id'])
 ##################################################################################################
 
 def test_channel_join_success():
-    clear()
-    token = Benjamin['token']
-    channel_id = 0
-    assert channel_join(token, channel_id) == {}
+    assert channel_join(Ross['token'], channel_id0) == {}
 
 def test_channel_join_invalid_channel():
-    clear()
     #The channel doesn't exist
     #This should throw InputError
-    token = Benjamin['token']
-    channel_id = 2
     with pytest.raises(error.InputError):
-        assert channel_join(token, channel_id)
+        assert channel_join(Benjamin['token'], 2)
 
 def test_channel_join_not_a_member():
-    clear()
     #Channel is private i.e. user is not admin
     #This should throw AccessError
-    token = Benjamin['token']
-    channel_id = 1
     with pytest.raises(error.AccessError):
-        assert channel_join(token, channel_id)
+        assert channel_join(Benjamin['token'], channel_id1)
