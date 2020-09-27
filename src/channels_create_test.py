d99578a7b52ea986e44a6ec117e870ee67fe29d3
Dylan Ngo <dylan.ngo09879@gmail.com>
2020-09-27 20:59:31 +1000

    Style Changes

--- src/channels_create_test.py
+++ src/channels_create_test.py
@@ -8,7 +8,7 @@ from auth import auth_register
 import error
 import pytest
 
-Jeffo = auth_register("Jeffo@email.com", "a1b2c3", "Jeffo", "Jeff")
+Dylan = auth_register("Jeffo@email.com", "a1b2c3", "Dylan", "Jeff")
 
 channel_name1 = "Main Channel"
 channel_name2 = "abcdefhijklmnopqrst"
