5b376d4c7b5d8adfb4635025c3e255b9508c3d39
Pennie <z5201973@student.unsw.edu.au>
2020-09-27 22:43:11 +1000

    fixed assertion error

--- src/auth_logout_test.py
+++ src/auth_logout_test.py
@@ -9,15 +9,18 @@ def test_auth_logout():
 
     # deactivate("validregistered@email.com") @Taimoor - what does this function do?
     
-    auth_dictionary = auth_register("validregistered@email.com", "1password", "Sam", "Drake")
-    user_token = auth_dictionary["token"]
+    auth_dictionary0 = auth_register("validregistered@email.com", "1password", "Sam", "Drake")
+    auth_dictionary1 = auth_register("validregistered@email.com", "2password", "Drake", "Sam")
+    user_token0 = auth_dictionary0["token"]
+    user_token1 = auth_dictionary1["token"]
+    
     invalid_token = "invalidtoken" # don't know if I need this
 
     # Token is invalid token
     assert auth_logout(invalid_token) == {'is_success': False} # don't know if I need this
 
     # When the token is valid 
-    assert auth_logout(user_token) == {'is_success': True}
+    assert auth_logout(user_token0) == {'is_success': True}
 
     #When token is already logged out
-    assert auth_logout(user_token) == {'is_success': False}
+    assert auth_logout(user_token1) == {'is_success': False}
