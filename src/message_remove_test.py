c805c81fcb84191d0a4a5171b58aa490cb9524b4
Dylan Ngo <dylan.ngo09879@gmail.com>
2020-09-27 23:17:48 +1000

    Edited copy and paste mistakes

--- src/message_remove_test.py
+++ src/message_remove_test.py
@@ -14,31 +14,31 @@ Jeffo = auth_register("Jeffo@email.com", "a1b2c3", "Jeffo", "Jeff")
 Smith = auth_register("smith@email.com", "a1b2c3", "Smith", "Smith")
 Tom = auth_register("tom@email.com", "a1b2c3", "Tom", "Tommery")
 
-channels_create(Dylan['token'], "Main Channel", True)
-channel_invite(Dylan['token'], 0, 0)
+channels_create(Jeffo['token'], "Main Channel", True)
+channel_invite(Jeffo['token'], 0, 0)
 channel_invite(Smith['token'], 0, 1)
 channel_invite(Tom['token'], 0, 2)
-channel_addowner(Dylan['token'], 0, 0)
+channel_addowner(Jeffo['token'], 0, 0)
 
 message1 = "Let's geddit"
-message_send(Dylan['token'], 0, message1)
+message_send(Jeffo['token'], 0, message1)
 message2 = "Shut up rat"
 message_send(Smith['token'], 0, message2)
 message3 = "Mb lmao"
-message_send(Dylan['token'], 0, message3)
+message_send(Jeffo['token'], 0, message3)
 message4 = "I hate you"
-message_send(Dylan['token'], 0, message4)
-message_remove(Dylan['token'], 3)
+message_send(Jeffo['token'], 0, message4)
+message_remove(Jeffo['token'], 3)
 
 def test_message_remove_firstmessage():
-    assert message_remove(Dylan['token'], 0) == {}
+    assert message_remove(Jeffo['token'], 0) == {}
 
 def test_message_remove_ownerremove():
-    assert message_remove(Dylan['token'], 1) == {}
+    assert message_remove(Jeffo['token'], 1) == {}
 
 def test_message_remove_messagenolonderexists():
     with pytest.raises(error.InputError):
-        assert message_remove(Dylan['token'], 3)
+        assert message_remove(Jeffo['token'], 3)
 
 def test_message_remove_messagedoesnotbelongtouser():
     with pytest.raises(error.AccessError):
--- src/test_search.py
+++ src/test_search.py
@@ -2,32 +2,30 @@
 Query String returns the collection of messages 
 '''
 
-from channel import channel_invite, channel_addowner
+from channel import channel_invite
 from channels import channels_create
 from auth import auth_register
-from message import message_send, message_remove
+from message import message_send
 from other import search
 import error
 import pytest
 
-Dylan = auth_register("JeffoD@email.com", "a1b2c3", "Dylan", "Jeff")
-channels_create(Dylan['token'], "Main Channel", True)
-channel_invite(Dylan['token'], 0, 0)
+Jeffo = auth_register("JeffoD@email.com", "a1b2c3", "Jeffo", "Jeff")
+channels_create(Jeffo['token'], "Main Channel", True)
+channel_invite(Jeffo['token'], 0, 0)
 message1 = "Let's geddit"
-message_send(Dylan['token'], 0, message1)
+message_send(Jeffo['token'], 0, message1)
 message2 = "Shut up rat"
-message_send(Dylan['token'], 0, message2)
+message_send(Jeffo['token'], 0, message2)
 message3 = "Mb lmao"
-message_send(Dylan['token'], 0, message3)
+message_send(Jeffo['token'], 0, message3)
 message4 = "Shut up ye"
-message_send(Dylan['token'], 0, message4)
+message_send(Jeffo['token'], 0, message4)
 
-def search_emptystring():
-    assert search(Dylan['token'], "") == ""
 
 def search_onestring():
-    assert search(Dylan['token'], "Let's") == "Let's geddit"    
+    assert search(Jeffo['token'], "Let's") == "Let's geddit"    
 
 def search_multiplestring():
-    assert search(Dylan['token'], "Shut") == "Shut up rat", "Shut up ye"
+    assert search(Jeffo['token'], "Shut") == "Shut up rat", "Shut up ye"
 
