6021cb3ec1799b1a46210bea0adf03287a4a1e7a
Taimoor Shabih <z5206259@ad.unsw.edu.au>
2020-09-28 21:22:41 +1000

    started auth func implementation

--- src/auth.py
+++ src/auth.py
@@ -1,3 +1,5 @@
+import re 
+
 def auth_login(email, password):
     return {
         'u_id': 1,
@@ -10,7 +12,45 @@ def auth_logout(token):
     }
 
 def auth_register(email, password, name_first, name_last):
-    return {
+    
+    # Password needs to be at least 6 characters long
+    if len(password) < 6:
+        raise InputError('Password entered is less than 6 characters long')
+
+
+    email_match = r'^\w+([\.-]?\w+)*@\w([\.-]?\w+)*(\.\w{2,3})+$'
+    if not re.search(email_match, email): # If it returns FALSE
+        raise InputError('Entered email is not valid')
+
+    first_name_length= len(name_first)
+    last_name_length= len(name_last)
+
+    # First name doesn't contain at least 1 character
+    if len(name_first) < 1:
+        raise InputError('First name is less than 1 character long')
+
+    # First name contains more than 50 characters
+    if len(name_first) > 50:
+        raise InputError('First name is more than 50 characters long')
+
+    if len(name_last) < 1:
+        raise InputError('Last name is less than 1 character long')
+
+    if len(name_last) > 50: 
+        raise InputError('Last name is more than 50 characters long')
+
+    # Check if email already registered
+    for registered_user in backend_database['users']:
+        if registered_user['email'] == email:
+            raise InputError('Email already taken by another registered user')
+
+
+    
+
+
+
+    return user_dictionary
+    '''return {
         'u_id': 1,
         'token': '12345',
-    }
+    }'''
