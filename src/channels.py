ddce0c9871ad9936e38c42e3aa27105aec71f259
Dylan Ngo <dylan.ngo09879@gmail.com>
2020-09-29 17:01:39 +1000

    Added channels_create function

--- src/channels.py
+++ src/channels.py
@@ -19,6 +19,15 @@ def channels_listall(token):
     }
 
 def channels_create(token, name, is_public):
+    if len(string) > 20:
+        raise error.InputError('Channel name is more than 20 characters')
+    else:
+        for channels in data['channels']:
+             if token == users['token']:
+                 for users in data['users']
+                    if token == users['token']:
+                        channel_id = len(backend_data['channels'])
+                        data['channels'].append({'channel_id': channel_id, 'name': name, 'is_public': is_public, 'owner_members': users, 'all_members': users})
+                        return {channel_id}
     return {
-        'channel_id': 1,
     }
