40ff9358593e8fe0affa8e9b6abb7903c2b01d05
Pennie <z5201973@student.unsw.edu.au>
2020-10-01 23:47:10 +1000

    updated setemail tests structure

--- src/user_profile_setemail_test.py
+++ src/user_profile_setemail_test.py
@@ -10,39 +10,44 @@ import auth
 import pytest
 import re
 
-def test_user_profile_setemail():
-	
-		clear()
+# Register 2 test users
+test_user_0 = auth.auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")      
+test_user_1 = auth.auth_register("test_email_1@email.com", "valid_pw1", "Jayden", "Haycobs")      
 
-		# Register 2 test users
-		test_user_0 = auth.auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")      
-		test_user_1 = auth.auth_register("test_email_1@email.com", "valid_pw1", "Jayden", "Haycobs")      
-		
-		# Get test users' tokens
-		test_user0_token = test_user_0['token']	
-		test_user1_token = test_user_1['token']	
+# Get test users' tokens
+test_user0_token = test_user_0['token']	
+test_user1_token = test_user_1['token']	
 
-		# Get test users' u_id
-		test_user0_id = test_user_0['u_id']
-		test_user1_id = test_user_1['u_id']
-		
-		# Invalid email address input - no '@' character		
-		with pytest.raises(InputError):
-				user_profile_setemail(test_user0_token, 'asdsad.com')
-		
-		assert emailCheck('test_email_0@email.com') == True
-		assert emailCheck('invalid.com') == False
+# Get test users' u_id
+test_user0_id = test_user_0['u_id']
+test_user1_id = test_user_1['u_id']
 
-		
-		# Invalid email - existing user's email
-		test_user1_profile = user_profile(test_user1_token, test_user1_id)
-		with pytest.raises(InputError):
-				user_profile_setemail(test_user0_token, test_user1_profile['email'])
+test_user1_profile = user_profile(test_user1_token, test_user1_id)
 
-		# Assert that email set works
-		user_profile_setemail(test_user0_token, "cs1531@cse.unsw.edu.au")
-		test_user0_updated = user_profile(test_user0_token, test_user0_id)
-		assert test_user0_updated['email'] == "cs1531@cse.unsw.edu.au"
+# check for invalid email - setting an existing user's email
+def test_user_profile_setemail_existing():
+    
+	with pytest.raises(InputError):
+		user_profile_setemail(test_user0_token, test_user1_profile['email'])
+
+# check for invalid email address input - no '@' character		
+def test_user_profile_setemail_invalid_check():
+    
+    with pytest.raises(InputError):
+        user_profile_setemail(test_user0_token, 'asdsad.com')
+
+# check for invalid email address input using email check helper function
+def test_user_profile_setemail_invalid_check_helper():
+            
+    assert emailCheck('test_email_0@email.com') == True
+    assert emailCheck('invalid.com') == False
+		
+# assert that email set works
+def test_user_profile_setemail_works():
+		
+    user_profile_setemail(test_user0_token, "cs1531@cse.unsw.edu.au")
+    test_user0_updated = user_profile(test_user0_token, test_user0_id)
+    assert test_user0_updated['email'] == "cs1531@cse.unsw.edu.au"
 
 
 # Modified email validity checker from https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
@@ -54,6 +59,5 @@ def emailCheck(email):
 		if(re.search(regex,email)):  
 				return True  
 					
-		else:  
-				return False
+		return False
 				
