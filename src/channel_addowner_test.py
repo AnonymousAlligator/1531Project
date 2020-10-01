00ba099faabbe28ce39e2603388433975ad1941b
Effie Hong <e.hong3648@gmail.com>
2020-10-01 23:39:43 +1000

    completed all tests for addowner

--- src/channel_addowner_test.py
+++ src/channel_addowner_test.py
@@ -1,81 +1,96 @@
 import pytest
 import error
 from auth import auth_register
-from channel import channel_details, channel_addowner, channel_invite
+from channel import channel_details, channel_addowner, channel_invite, channel_join
 from channels import channels_create
 
 
 #Setting up data files:
 #user_0 is flockowner
+#Users 1, 2, 3 are part of a public channel
+#User 5 is owner of a private channel
+#User 6 is a member of a private channel
 user_0 = auth_register("apples@email.com", "applepass", "apple", "red") #returns { u_id, token }
-#user_1 is owner of channel
 user_1 = auth_register("banana@email.com", "bananapass", "banana", "yellow") #returns { u_id, token }
-#user_2 is not owner of channel
 user_2 = auth_register("carrot@email.com", "carrotpass", "carrot", "orange") #returns { u_id, token }
-#user_3 is not a member of the channel
 user_3 = auth_register("durian@email.com", "durianpass", "durian", "yellow")
-#user_4 is member of the channel (used for successes) 
-user_4 = auth_register("eggplant@email.com", "eggplantpass", "eggplant", "purple")
+user_5 = auth_register("figs@email.com", "figspass", "figs", "green")
+user_6 = auth_register("grape@email.com", "grapepass", "grape", "green")
 
-#create public channel to join 
+#Create public channel to join 
 channel_0 = channels_create(user_0['token'], "name", True) #returns channel ID_0
-#creates private channel to join
-channel_1 = channels_create(user_0['token'], "name", False) #returns channel ID_1
+#Creates private channel to join
+channel_1 = channels_create(user_5['token'], "name", False) #returns channel ID_1
 
-#invites user_0 to the private channel
-channel_invite(user_0['token'],channel_1)
-channel_invite(user_1['token'], channel_1)
+#Users 1, 2 ,3 are part of a public channel
+channel_invite(user1['token', channel_0])
+channel_invite(user2['token', channel_0])
+channel_invite(user3['token', channel_0])
 
-#Details owner of channel
-channel_details_0 = channel_details(user_2['token'],channel_0) #returns {name,owner_,members, all_members}
-
+#User 6 is a member of a private channel
+channel_join(user_6['token'], channel_1)
 
 ################################################################################################
 #Make user with user id u_id an owner of this channel
 
-#Channel exists, public channel, token is an owner, u_id is a member, 
+#Channel exists, token is an owner, u_id is a member, 
 def test_channel_addowner_sucess():
     token = user_0['token']
     u_id = user_1['u_id']
     channel_id = channel_0
     assert channel_addowner(token, channel_id, u_id) == {}
 
-#Channel exists, public channel, token is not an owner, u_id is a member
-#Channel exists, public channel, token is an owner, u_id is not a member
-#Channel exists, private channel, token not an owner, u_id is a not a member
-#Channel exists, public channel, token is flock owner, uid is a member
-#Channel exists, public channel, token is flock owner, uid is a member
+#Channel exists, token is NOT an owner, u_id is a member
+def test_channel_addowner_not_owner():
+    token = user_3['token']
+    u_id = user_2['u_id']
+    channel_id = channel_0
+    with pytest.raises(error.AccessError):
+        assert channel_addowner(token, channel_id, u_id) == {}
 
-#Channel does not exist, throw input error
-def test_channel_addowner_invalid_channel(): 
+#Channel exists, token is NOT a member, u_id is a member
+def test_adder_not_member():
+    token = user_4['token']
+    u_id = user_2['u_id']
+    channel_id = channel_0
+    with pytest.raises(error.AccessError):
+        assert channel_addowner(token, channel_id, u_id) == {}
+
+#Channel exists, token is owner adding someone who is already a owner
+def test_channel_addowner_already_owner():
     token = user_0['token']
     u_id = user_1['u_id']
-    channel_id = None
-    with pytest.raises(error.InputError):
-        assert channel_addowner(token, channel_id, u_id) == {}
-#Caller is not an owner
-
-
-#Channel exists, public channel, token is owner but u_id is owner.
-#NEEDS FIXING
-def test_channel_addowner_already_owner():
-    token = user_2['token']
-    u_id = user_3['u_id']
     channel_id = channel_0
     with pytest.raises(error.InputError):
         assert channel_addowner(token, channel_id, u_id) == {}
 
-#user 0 is part of private channel(channel_1)
-def test_channel_addowner_invited():
+#Channel exists, token is adding themselves.
+def test_channel_addowner_self():
+    token = user_0['token']
+    u_id = user_0['u_id']
+    channel_id = channel_0
+    with pytest.raises(error.InputError):
+        assert channel_addowner(token, channel_id, u_id) == {}
+
+#Channel does NOT exist, throw input error
+def test_channel_addowner_invalid_channel(): 
     token = user_0['token']
     u_id = user_1['u_id']
+    channel_id = 4
+    with pytest.raises(error.InputError):
+        assert channel_addowner(token, channel_id, u_id) == {}
+
+#Channel is private, user is part of private channel 
+def test_channel_addowner_invited():
+    token = user_5['token']
+    u_id = user_6['u_id']
     channel_id = channel_1
     assert channel_addowner(token, channel_id, u_id) == {}
 
-#user 1 is NOT part of private channel(channel_1)
-def test_channel_addowner_not_invited:
-    token = user_1['token']
-    u_id = user_1['u_id']
+#User 1 is NOT part of private channel
+def test_channel_addowner_not_invited():
+    token = user_5['token']
+    u_id = user_2['u_id']
     channel_id = channel_1
     with pytest.raises(error.AccessError):
         assert channel_addowner(token, channel_id, u_id) == {}
