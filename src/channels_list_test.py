4459034eb317c2e62383c8c35a1cb8479ffea430
Pennie <z5201973@student.unsw.edu.au>
2020-10-01 21:21:41 +1000

    added test data for channels_list_test

--- src/channels_list_test.py
+++ src/channels_list_test.py
@@ -1,18 +1,13 @@
-from channels import channels_list
-import error
+'''
+Provide a list of all channels (and their associated details) that the authorised user is part of
+'''
+from channels import channels_list, channels_create
+from auth import auth_register, auth_login
+# from other import clear #TODO: uncomment once merged into all_tests
 
+# register test users
+test_user0 = auth_register("test_0@email.com", "0password0", "Hayden", "Jacobs") #returns { u_id, token }
+test_user1 = auth_register("test_1@email.com", "1password1", "Jayden", "Haycobs") #returns { u_id, token }
 
-def test_channels_list_exists():
-    assert channels_list('token') == {channels}
-
-"""
-def channels_list(token):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
-"""
+test_user0_token = test_user0['token']
+test_user0_token = test_user1['token']
--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -1,32 +1,30 @@
 '''
-Provide a list of all channels (and their associated details) that the authorised user is part of
+Provide a list of all channels (and their associated details)
 '''
-from channels import channels_listall
-from auth_register import auth_register
-from auth_login import auth_login
-from channels_create import channels_create
-from other import clear
+from channels import channels_listall, channels_create
+from auth import auth_register, auth_login
+# from other import clear #TODO: uncomment once merged into all_tests
 
-# create users for testing
-user_0 = auth_register("test_0@email.com", "0password0", "Hayden", "Jacobs") #returns { u_id, token }
-user_1 = auth_register("test_1@email.com", "1password1", "Jayden", "Haycobs") #returns { u_id, token }
+# register test users
+test_user0 = auth_register("test_0@email.com", "0password0", "Hayden", "Jacobs") #returns { u_id, token }
+test_user1 = auth_register("test_1@email.com", "1password1", "Jayden", "Haycobs") #returns { u_id, token }
 
+test_user0_token = test_user0['token']
+test_user0_token = test_user1['token']
 
 
 # tests for listing one public channels
 def test_channels_listall_public():
-    clear()
-
+    # clear()
     
-
 # tests for listing one private channels
 def test_channels_listsall_private():
-    clear()
+    # clear()
 
 # tests for listing one of each public and private channels
 def test_channels_listsall_both():
-    clear()
+    # clear()
 
 # tests for listing many of each public and private channels
 def test_channels_listsall_many():
-    clear()
+    # clear()
