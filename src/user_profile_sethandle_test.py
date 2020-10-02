35d51ec7e203a69202ea60bfd6209aacb2043832
Pennie <z5201973@student.unsw.edu.au>
2020-10-03 03:16:39 +1000

    updated sethandle tests for user

--- src/user_profile_sethandle_test.py
+++ src/user_profile_sethandle_test.py
@@ -8,56 +8,56 @@ from user import user_profile, user_profile_sethandle
 from auth import auth_register
 from error import InputError
 from other import clear
+from test_helpers import create_one_test_user, create_two_test_users
 import pytest
 
-# clear previous test data
-clear() 
-
-# Register 2 users
-test_user_0 = auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")
-test_user_1 = auth_register("test_email_1@email.com", "valid_pw1", "Jayden", "Haycobs")
-
-# Get test users' tokens
-test_user0_token = test_user_0['token']
-test_user1_token = test_user_1['token']
-
-# Get test users' u_id
-test_user0_id = test_user_0['u_id']
-test_user1_id = test_user_1['u_id']
-
 # assert handle updates correctly
-def test_user_profile_sethandle_works():    
-    
+def test_user_profile_sethandle_works():
+
+    clear()
+    test_user0 = create_one_test_user()
+
     # update the test_user0's handle
-    user_profile_sethandle(test_user0_token, '1531_admin')    
+    user_profile_sethandle(test_user0['token'], '1531_admin')
     # get test_user0's profile
-    test_user0_updated = user_profile(test_user0_token, test_user0_id)
+    test_user0_updated = user_profile(test_user0['token'], test_user0['u_id'])
 
     assert test_user0_updated['handle_str'] == "1531_admin"
 
 # check for invalid handle string - str > 20 char in length
 def test_user_profile_sethandle_short():
-    with pytest.raises(InputError):
-        user_profile_sethandle(test_user0_token, "A" * 21)
+
+    clear()
+    test_user0, test_user1 = create_two_test_users()
 
     with pytest.raises(InputError):
-        user_profile_sethandle(test_user0_token, "A" * 200)
+        user_profile_sethandle(test_user0['token'], "A" * 21)
+
+    with pytest.raises(InputError):
+        user_profile_sethandle(test_user1['token'], "A" * 200)
 
 # check for invalid handle string - str < 3 char in length
 def test_user_profile_sethandle_long():
-    with pytest.raises(InputError):
-        user_profile_sethandle(test_user0_token, "A")    
+    
+    clear()
+    test_user0, test_user1 = create_two_test_users()
 
     with pytest.raises(InputError):
-        user_profile_sethandle(test_user0_token, "aa")    
+        user_profile_sethandle(test_user0['token'], "A")
+
+    with pytest.raises(InputError):
+        user_profile_sethandle(test_user1['token'], "aa")
 
 # check for invalid handle string - handle already exists
 def test_user_profile_sethandle_exists():
-
+    
+    clear()
+    test_user0, test_user1 = create_two_test_users()
+    
     # get test_user1's profile
-    test_user1_profile = user_profile(test_user1_token, test_user1_id)
+    test_user1_profile = user_profile(test_user1['u_id'], test_user1['u_id'])
 
     with pytest.raises(InputError):
-        user_profile_sethandle(test_user0_token, test_user1_profile['handle_str'])
+        user_profile_sethandle(test_user0['token'], test_user1_profile['handle_str'])
 
 
