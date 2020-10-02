2fc9101ea5430f945b598e67e053db7197ae1268
Pennie <z5201973@student.unsw.edu.au>
2020-10-03 00:08:54 +1000

    test moving clear

--- src/user_profile_setemail_test.py
+++ src/user_profile_setemail_test.py
@@ -11,7 +11,7 @@ import pytest
 import re
 
 # clear previous test data
-clear()
+# clear()
 
 # Register 2 test users
 test_user_0 = auth.auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")      
@@ -30,33 +30,33 @@ test_user1_profile = user_profile(test_user1_token, test_user1_id)
 
 # assert that email set works
 def test_user_profile_setemail_works():
-		
+	clear()	
     user_profile_setemail(test_user0_token, "cs1531@cse.unsw.edu.au")
     test_user0_updated = user_profile(test_user0_token, test_user0_id)
     assert test_user0_updated['email'] == "cs1531@cse.unsw.edu.au"
 
 # check for invalid email - setting an existing user's email
 def test_user_profile_setemail_existing():
-    
+    clear()	
 	with pytest.raises(InputError):
 		user_profile_setemail(test_user0_token, test_user1_profile['email'])
 
 # check for invalid email address input - no '@' character		
 def test_user_profile_setemail_invalid_check():
-    
+    clear()	
     with pytest.raises(InputError):
         user_profile_setemail(test_user0_token, 'asdsad.com')
 
 # check for invalid email address input using email check helper function
 def test_user_profile_setemail_invalid_check_helper():
-            
+    clear()	        
     assert emailCheck('test_email_0@email.com') == True
     assert emailCheck('invalid.com') == False		
 
 
 # regex taken from https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
 def emailCheck(email):  
-	
+
 		regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
 		
         # pass the regular expression and the string in search() method 
