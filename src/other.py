3545c7270e7258ca7106e6d6282b7d5abae8911e
Kevin Hu <kevin.hu1226@gmail.com>
2020-10-02 23:35:32 +1000

    fixed syntax issues

--- src/other.py
+++ src/other.py
@@ -2,14 +2,14 @@ from error import AccessError
 
 data = {
 'users': [],
-'channels': [{
+'channels': [
     #id: 'channel_id'
     #name: 'channel_name'
     #is_public: True or False
     #owner_members: [{u_id, token},]
     #all_members: [{u_id, token},]
     #messages:[{message_id, u_id, message, time_created},]
-    }],
+    ],
 }
 
 
