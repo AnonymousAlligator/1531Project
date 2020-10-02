664c8cab2e1e35737171cae49d661ede3c9c3e44
Pennie <z5201973@student.unsw.edu.au>
2020-10-03 03:10:39 +1000

    updated user profile tests

--- src/user_profile_test.py
+++ src/user_profile_test.py
@@ -6,46 +6,46 @@ from user import user_profile
 from error import InputError
 from auth import auth_register
 from other import clear
+from test_helpers import create_one_test_user
 import pytest
 
-# clear previous test data
-clear()
-
-# Register 1 users
-test_user_0 = auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")
-
-# Get test user's tokens
-test_user0_token = test_user_0['token']
-
-# Get test user's u_id
-test_user0_id = test_user_0['u_id']
 
 # check that a valid token and valid u_id returns the correct profile data
-def test_user_profile():		
-		assert(user_profile(test_user0_token, test_user0_id) == {
-				'u_id': test_user0_id,
-				'email': 'cs1531@cse.unsw.edu.au',
-				'name_first': 'Hayden',
-				'name_last': 'Jacobs',
-				'handle_str': 'hjacobs',
-		})
+def test_user_profile():	
+    clear()	
+
+    test_user0 = create_one_test_user()
+
+    assert(user_profile(test_user0['token'], test_user0['u_id']) == {
+        'u_id': test_user0['u_id'],
+        'email': 'cs1531@cse.unsw.edu.au',
+        'name_first': 'Hayden',
+        'name_last': 'Jacobs',
+        'handle_str': 'hjacobs',
+    })
 
 # check for invalid token with a valid u_id
 def test_user_profile_invalid_token():
-		
-		with pytest.raises(InputError):
-				user_profile('invalid_token', test_user0_id)
-		
-		with pytest.raises(InputError):
-				user_profile(12345678, test_user0_id)
+    clear()	
+
+    test_user0 = create_one_test_user()
+
+    with pytest.raises(InputError):
+        user_profile('invalid_token', test_user0['u_id'])
+    
+    with pytest.raises(InputError):
+        user_profile(12345678, test_user0['u_id'])
 
 
 # check for invalid u_id with a token
 def test_user_profile_invalid_uid():
+    clear()	
+    
+    test_user0 = create_one_test_user()
 
-		with pytest.raises(InputError):
-				user_profile(test_user0_token, 100)
-		
-		with pytest.raises(InputError):
-				user_profile(test_user0_token, 2)
+    with pytest.raises(InputError):
+        user_profile(test_user0['token'], 100)
+    
+    with pytest.raises(InputError):
+        user_profile(test_user0['token'], 2)
 
