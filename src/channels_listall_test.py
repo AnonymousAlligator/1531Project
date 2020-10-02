c3395a00f60e99b95fd209baf3aafb491eec1bbf
Pennie <z5201973@student.unsw.edu.au>
2020-10-02 23:32:51 +1000

    updated channels_listall_test

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -4,7 +4,9 @@ NOTE: assumption that it returns all public/private channels regardless of user 
 '''
 from channels import channels_listall, channels_create
 from auth import auth_register, auth_login
-# from other import clear #TODO: uncomment once merged into all_tests
+from other import clear 
+
+clear()
 
 # register test users
 test_user0 = auth_register("test_0@email.com", "0password0", "Hayden", "Jacobs") #returns { u_id, token }
@@ -15,8 +17,7 @@ test_user1_token = test_user1['token']
 
 # tests for listing one public channels
 def test_channels_listall_public():
-    # clear()
-
+    
     # test_user0 creates 1 public channel
     pub_channel0 = channels_create(test_user0_token, "Public  Channel 0", True)
     pub_channel0_id = pub_channel0['channel_id']
@@ -30,7 +31,6 @@ def test_channels_listall_public():
 
 # tests for listing one private channels
 def test_channels_listsall_private():
-    # clear()
 
     # test_user0 creates 1 private channel
     priv_channel0 = channels_create(test_user0_token, "Private Channel 0", False)
@@ -45,7 +45,6 @@ def test_channels_listsall_private():
 
 # tests for listing one of each public and private channels
 def test_channels_listsall_both():
-    # clear()
 
     # test_user0 creates 1 public channel
     pub_channel0 = channels_create(test_user0_token, "Public  Channel 0", True)
@@ -68,8 +67,6 @@ def test_channels_listsall_both():
 
 # tests for listing many of each public and private channels
 def test_channels_listsall_many():
-    # clear()
-
     # create multiple test channels
 
     # test_user0 creates 1 public channel
