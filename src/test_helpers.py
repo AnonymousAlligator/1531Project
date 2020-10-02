63f7f8a47c4636596b3a08ff6aa01b5454799e70
Pennie <z5201973@student.unsw.edu.au>
2020-10-03 00:31:55 +1000

    added new test_helpers

---
+++ src/test_helpers.py
@@ -0,0 +1,17 @@
+
+def create_one_test_user():
+    # Register 1 users
+    return auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")
+
+def create_two_test_users():
+    # Register 2 users
+        test_user_0 = auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")
+        test_user_1 = auth_register("test_email_1@email.com", "valid_pw1", "Jayden", "Haycobs")
+
+    test_users = {
+        'user1': test_user_0,
+        'user2': test_user_1,
+    }
+  
+    return test_users  
+
--- src/user_profile_setname_test.py
+++ src/user_profile_setname_test.py
@@ -9,32 +9,39 @@ from user import user_profile_setname, user_profile
 from error import InputError
 from auth import auth_register
 from other import clear
+from test_helpers import create_one_test_user, create_two_test_user
 import pytest
 
 # clear previous test data
 # clear() 
 
 # Register 1 users
-test_user_0 = auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")
+# test_user_0 = auth_register("test_email_0@email.com", "valid_pw0", "Hayden", "Jacobs")
 
-# Get test user's tokens
-test_user0_token = test_user_0['token']
+# # Get test user's tokens
+# test_user0_token = test_user_0['token']
 
-# Get test user's u_id
-test_user0_id = test_user_0['u_id']
+# # Get test user's u_id
+# test_user0_id = test_user_0['u_id']
 
 
 # check for correct name update
 def test_profile_setname():
     clear()    
-    user_profile_setname(test_user0_token, "Jayden", "Haycob")    
-    updated_test_user_0 = user_profile(test_user0_token, test_user_0['u_id'])    
-    assert updated_test_user_0['name_first'] == "Jayden"
-    assert updated_test_user_0['name_last'] == "Haycob"
+
+    test_user0 = create_one_test_user()
+
+    user_profile_setname(test_user0['token'], "Nick", "Smith")    
+    updated_test_user_0 = user_profile(test_user0['token'], test_user_0['u_id'])    
+    assert updated_test_user_0['name_first'] == "Nick"
+    assert updated_test_user_0['name_last'] == "Smith"
 
 # check for invalid token
 def test_profile_setname_invalid_token():
     clear()
+    
+    create_one_test_user()
+
     with pytest.raises(InputError):
         user_profile_setname('invalid_token', "Jayden", "Haycob")
 
@@ -44,28 +51,32 @@ def test_profile_setname_invalid_token():
 # check for invalid first name input 
 def test_profile_setname_invalid_fname():    
     clear()
+    test_user0 = create_one_test_user()
+
     # Invalid firstname input - more than 50 characters
     with pytest.raises(InputError):
-        user_profile_setname(test_user0_token, "A" * 51, "valid_new_lname")
+        user_profile_setname(test_user0['token'], "A" * 51, "valid_new_lname")
     
     with pytest.raises(InputError):
-        user_profile_setname(test_user0_token, "A" * 100, "valid_new_lname")
+        user_profile_setname(test_user0['token'], "A" * 100, "valid_new_lname")
     
     # Invalid first name input - input is 1 character
     with pytest.raises(InputError):
-        user_profile_setname(test_user0_token, "A", "valid_new_lname")
+        user_profile_setname(test_user0['token'], "A", "valid_new_lname")
 
 # check for invalid last name input 
 def test_profile_setname_invalid_lname():        
     clear()
+    test_user0 = create_one_test_user()
+    
     # Invalid last name input - more than 50 characters
     with pytest.raises(InputError):
-        user_profile_setname(test_user0_token, "valid_new_fname", "A" * 51)
+        user_profile_setname(test_user0['token'], "valid_new_fname", "A" * 51)
     
     with pytest.raises(InputError):
-        user_profile_setname(test_user0_token, "valid_new_fname", "A" * 100)
+        user_profile_setname(test_user0['token'], "valid_new_fname", "A" * 100)
     
     # Invalid last name input - input is 1 character
     with pytest.raises(InputError):
-        user_profile_setname(test_user0_token, "valid_new_fname", "A" )
+        user_profile_setname(test_user0['token'], "valid_new_fname", "A" )
 
