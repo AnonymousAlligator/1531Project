67af3ffdedfe44b145b0d3cd1ca4b3d6eb798ac2
Pennie <z5201973@student.unsw.edu.au>
2020-10-03 03:24:22 +1000

    updated setemail test

--- src/user_profile_setemail_test.py
+++ src/user_profile_setemail_test.py
@@ -13,28 +13,43 @@ import re
 
 # assert that email set works
 def test_user_profile_setemail_works():
+    
     clear()
-    user_profile_setemail(test_user0_token, "cs1531@cse.unsw.edu.au")
-    test_user0_updated = user_profile(test_user0_token, test_user0_id)
+    test_user0 = create_one_test_user()
+
+    user_profile_setemail(test_user0['token'], "cs1531@cse.unsw.edu.au")
+    test_user0_updated = user_profile(test_user0['token'], test_user0['u_id'])
     assert test_user0_updated['email'] == "cs1531@cse.unsw.edu.au"
 
 # check for invalid email - setting an existing user's email
 def test_user_profile_setemail_existing():
-    clear()	
+    
+    clear()    
+    test_user0, test_user1 = create_two_test_users()
+
+    test_user1_profile = user_profile(test_user1['token'], test_user1['u_id'])
+
     with pytest.raises(InputError):
-        user_profile_setemail(test_user0_token, test_user1_profile['email'])
+        user_profile_setemail(test_user0['token'], test_user1_profile['email'])
 
 # check for invalid email address input - no '@' character		
 def test_user_profile_setemail_invalid_check():
-    clear()	
+    
+    clear()
+    test_user0 = create_one_test_user()
+
     with pytest.raises(InputError):
-        user_profile_setemail(test_user0_token, 'asdsad.com')
+        user_profile_setemail(test_user0['token'], 'asdsad.com')
 
 # check for invalid email address input using email check helper function
 def test_user_profile_setemail_invalid_check_helper():
-    clear()	        
+    
+    #TODO: check this one
+    # clear()
+    # test_user0 = create_one_test_user()
+
     assert emailCheck('test_email_0@email.com') == True
-    assert emailCheck('invalid.com') == False		
+    assert emailCheck('invalid.com') == False	    
 
 
 # regex taken from https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
