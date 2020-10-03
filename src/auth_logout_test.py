696900ff789e5a8a14570244d6086b063f557327
Taimoor Shabih <z5206259@ad.unsw.edu.au>
2020-10-04 01:27:51 +1000

    fully functional afaik

--- src/auth_logout_test.py
+++ src/auth_logout_test.py
@@ -7,12 +7,12 @@ def test_auth_logout():
 
     clear()
 
-    # deactivate("validregistered@email.com") @Taimoor - what does this function do?
-    
     auth_dictionary0 = auth_register("validregistered@email.com", "1password", "Sam", "Drake")
-    
+    auth_dictionary1 = auth_register("validregistered1@email.com", "1password", "Sam", "Drake")
     
     # When the token is valid 
     assert auth_logout(auth_dictionary0['token']) == {'is_success': True}
-
+    assert auth_logout(auth_dictionary1['token']) == {'is_success': True}
     
+    #assert auth_logout(auth_dictionary0['token']) == {'is_success': False}
+    #assert auth_logout(auth_dictionary1['token']) == {'is_success': False}
