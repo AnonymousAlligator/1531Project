a2038ef3740d3b2454c5e7333fac92758c500f14
Effie Hong <e.hong3648@gmail.com>
2020-10-03 20:09:54 +1000

    added helper funcs to removeonwner tests

--- src/channel_removeowner_test.py
+++ src/channel_removeowner_test.py
@@ -12,47 +12,7 @@ from other import clear
 #User 4 is not part of any channel
 #User 5 is owner of a private channel
 #User 6 is a member of a private channel
-user_0 = auth_register("apples@email.com", "applepass", "apple", "red") #returns { u_id, token }
-user_1 = auth_register("banana@email.com", "bananapass", "banana", "yellow") #returns { u_id, token }
-user_2 = auth_register("carrot@email.com", "carrotpass", "carrot", "orange") #returns { u_id, token }
-user_3 = auth_register("durian@email.com", "durianpass", "durian", "yellow")
-user_4 = auth_register("eggplant@email.com", "eggplantpass", "eggplant", "purple")
-user_5 = auth_register("figs@email.com", "figspass", "figs", "green")
-user_6 = auth_register("grape@email.com", "grapepass", "grape", "green")
 
-
-#Create first public channel 
-channel_0 = channels_create(user_0['token'], "name", True) #returns channel ID_0
-
-#Users 1, 2 ,3 are part of a public channel
-channel_invite(user_0['token'], channel_0, user_1['u_id'])
-channel_invite(user_0['token'], channel_0, user_2['u_id'])
-channel_invite(user_0['token'], channel_0, user_3['u_id'])
-channel_invite(user_0['token'], channel_0, user_4['u_id'])
-channel_invite(user_0['token'], channel_0, user_5['u_id'])
-
-channel_addowner(user_0['token'], channel_0, user_1['u_id'])
-channel_addowner(user_0['token'], channel_0, user_2['u_id'])
-
-#Creating second public channel
-channel_1 = channels_create(user_1['token'], "Multiple owner", True) #returns channel ID_0
-channel_join(user_2['token'], channel_1)
-channel_addowner(user_1['token'], channel_1, user_2['u_id'])
-
-#Creating third public channel
-channel_3 = channels_create(user_1['token'], "Single owner,two mem", True) #returns channel ID_0
-channel_join(user_2['token'], channel_1)
-
-#Creating fourth public channel
-channel_4 = channels_create(user_1['token'], "Single owner,two mem", True) #returns channel ID_0
-
-#data files for private channel checks
-channel_2 = channels_create(user_1['token'], "name", False) #returns channel ID_0
-channel_invite(user_1['token'], channel_2, user_2['u_id'])
-channel_addowner(user_1['token'], channel_2, user_2['u_id'])
-
-#data files for private channel checks
-channel_5 = channels_create(user_0['token'], "name", False) #returns channel ID_0
 ########################################################################################
 def test_channel_removeowner_success():
     clear()
