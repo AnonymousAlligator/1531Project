c30bc164ced2baed02e5a6e20967145158e1103d
Pennie <z5201973@student.unsw.edu.au>
2020-10-03 16:59:23 +1000

    updated message_remove_test

--- src/message_remove_test.py
+++ src/message_remove_test.py
@@ -8,13 +8,9 @@ from channels import channels_create
 from auth import auth_register
 from message import message_send, message_remove
 from test_helpers import create_one_test_user, create_two_test_users, create_three_test_users
-from error import InputError
+from error import AccessError, InputError
 from other import clear
 import pytest
-
-# Jeffo = auth_register("Jeffo@email.com", "a1b2c3", "Jeffo", "Jeff")
-# Smith = auth_register("smith@email.com", "a1b2c3", "Smith", "Smith")
-# Tom = auth_register("tom@email.com", "a1b2c3", "Tom", "Tommery")
 
 # check func works when 1 user (owner) in public channel, sends 1 message and removes it
 def test_message_remove_works_public():
@@ -26,8 +22,8 @@ def test_message_remove_works_public():
     public_channel_id = channels_create(test_user0['token'], "Public Channel", True)
         
     # test_user0 sends 1 message to public channel
-    message0 = "Let's geddit"
-    message_send(test_user0['token'], public_channel_id, message0)
+    message0_id = "Let's geddit"
+    message_send(test_user0['token'], public_channel_id, message0_id)
 
     # assert func works for public channel    
     assert message_remove(test_user0['token'], 1) == {} # TODO: update in iteration2
@@ -42,8 +38,8 @@ def test_message_remove_works_private():
     private_channel_id = channels_create(test_user0['token'], "Private Channel", False)
         
     # test_user0 sends 1 message to public channel
-    message0 = "Let's geddit"
-    message_send(test_user0['token'], private_channel_id, message0)
+    message0_id = "Let's geddit"
+    message_send(test_user0['token'], private_channel_id, message0_id)
 
     # assert func works for private channel    
     assert message_remove(test_user0['token'], 1) == {} # TODO: update in iteration2    
@@ -61,21 +57,53 @@ def test_message_remove_works_more_people_channel():
     channel_join(test_user1['token'], public_channel_id)
         
     # test_user1 sends 1 message
-    message0 = "Let's geddit"
-    message_send(test_user1['token'], 0, message0)
+    message0_id = "Let's geddit"
+    message_send(test_user1['token'], public_channel_id, message0_id)
 
     # check test_user1 (member) removes their message
     assert message_remove(test_user1['token'], 1) == {} # TODO: update in iteration2    
 
-# TODO
-# def test_message_remove_messagenolonderexists():
-#     with pytest.raises(error.InputError):
-#         assert message_remove(Jeffo['token'], 3)
+# check user can't remove an already deleted message 
+def test_message_remove_already_deleted_msg():
 
-# def test_message_remove_messagedoesnotbelongtouser():
-#     with pytest.raises(error.AccessError):
-#         assert message_remove(Smith['token'], 0)
+    clear()
+    test_user0 = create_one_test_user()
 
-# def test_message_remove_isnotowner():
-#     with pytest.raises(error.AccessError):
-#         assert message_remove(Tom['token'], 0)
+    # test_user0 creates 1 private channel
+    private_channel_id = channels_create(test_user0['token'], "Private Channel", False)
+        
+    # test_user0 sends 1 message to public channel
+    message0_id = "Let's geddit"
+    message_send(test_user0['token'], private_channel_id, message0_id)
+
+    # test_user0 removes message
+    message_remove(test_user0['token'], 1) == {} # TODO: update in iteration2 
+
+    # TODO: update in iteration2 
+    # test_user0 tries to call remove for a deleted message    
+    # with pytest.raises(InputError):
+    #     message_remove(test_user0['token'], message0_id) == {}    
+    
+    pass 
+
+# check user0 can't remove user1's message
+def test_message_remove_user_remove_anothers():
+    clear()
+    test_user0, test_user1 = create_two_test_users()
+
+    # test_user0 creates 1 public channel
+    public_channel_id = channels_create(test_user0['token'], "Main Channel", True)
+
+    # test_user1 joins public channel 
+    channel_join(test_user1['token'], public_channel_id)
+        
+    # test_user1 sends 1 message
+    message0_id = "Let's geddit"
+    message_send(test_user1['token'], public_channel_id, message0_id)
+
+    # TODO: update in iteration2 
+    # test_user0 tries to remove test_user1's message
+    # with pytest.raises(AccessError):
+    #     message_remove(test_user0['token'], message0_id)
+    
+    pass
