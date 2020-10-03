306313295a9bf35539a780e4076b197c141f5193
Pennie <z5201973@student.unsw.edu.au>
2020-10-03 22:43:39 +1000

    skip tests for message tests

--- src/message_remove_test.py
+++ src/message_remove_test.py
@@ -20,12 +20,12 @@ def test_message_remove_works_public():
 
     # test_user0 creates 1 public channel
     public_channel_id = channels_create(test_user0['token'], "Public Channel", True)
-        
+
     # test_user0 sends 1 message to public channel
     message0_id = "Let's geddit"
     message_send(test_user0['token'], public_channel_id, message0_id)
 
-    # assert func works for public channel    
+    # assert func works for public channel
     assert message_remove(test_user0['token'], 1) == {} # TODO: update in iteration2
 
 @pytest.mark.skip(reason='function implementation not done yet')
@@ -37,36 +37,36 @@ def test_message_remove_works_private():
 
     # test_user0 creates 1 private channel
     private_channel_id = channels_create(test_user0['token'], "Private Channel", False)
-        
+
     # test_user0 sends 1 message to public channel
     message0_id = "Let's geddit"
     message_send(test_user0['token'], private_channel_id, message0_id)
 
-    # assert func works for private channel    
-    assert message_remove(test_user0['token'], 1) == {} # TODO: update in iteration2    
+    # assert func works for private channel
+    assert message_remove(test_user0['token'], 1) == {} # TODO: update in iteration2
 
 @pytest.mark.skip(reason='function implementation not done yet')
 # check func works when 2 users in channel, user1 (not owner) sends message and removes it
 def test_message_remove_works_more_people_channel():
-    
+
     clear()
     test_user0, test_user1 = create_two_test_users()
 
     # test_user0 creates 1 public channel
     public_channel_id = channels_create(test_user0['token'], "Main Channel", True)
 
-    # test_user1 joins public channel 
+    # test_user1 joins public channel
     channel_join(test_user1['token'], public_channel_id)
-        
+
     # test_user1 sends 1 message
     message0_id = "Let's geddit"
     message_send(test_user1['token'], public_channel_id, message0_id)
 
     # check test_user1 (member) removes their message
-    assert message_remove(test_user1['token'], 1) == {} # TODO: update in iteration2    
+    assert message_remove(test_user1['token'], 1) == {} # TODO: update in iteration2
 
 @pytest.mark.skip(reason='function implementation not done yet')
-# check user can't remove an already deleted message 
+# check user can't remove an already deleted message
 def test_message_remove_already_deleted_msg():
 
     clear()
@@ -74,18 +74,18 @@ def test_message_remove_already_deleted_msg():
 
     # test_user0 creates 1 private channel
     private_channel_id = channels_create(test_user0['token'], "Private Channel", False)
-        
+
     # test_user0 sends 1 message to public channel
     message0_id = "Let's geddit"
     message_send(test_user0['token'], private_channel_id, message0_id)
 
     # test_user0 removes message
-    message_remove(test_user0['token'], 1) == {} # TODO: update in iteration2 
+    message_remove(test_user0['token'], 1) == {} # TODO: update in iteration2
 
-    # test_user0 tries to call remove for a deleted message    
+    # test_user0 tries to call remove for a deleted message
     with pytest.raises(InputError):
-        message_remove(test_user0['token'], message0_id) == {}    
-    
+        message_remove(test_user0['token'], message0_id) == {}
+
 
 @pytest.mark.skip(reason='function implementation not done yet')
 # check user0 can't remove user1's message
@@ -96,9 +96,9 @@ def test_message_remove_user_remove_anothers():
     # test_user0 creates 1 public channel
     public_channel_id = channels_create(test_user0['token'], "Main Channel", True)
 
-    # test_user1 joins public channel 
+    # test_user1 joins public channel
     channel_join(test_user1['token'], public_channel_id)
-        
+
     # test_user1 sends 1 message
     message0_id = "Let's geddit"
     message_send(test_user1['token'], public_channel_id, message0_id)
@@ -106,4 +106,3 @@ def test_message_remove_user_remove_anothers():
     # test_user0 tries to remove test_user1's message
     with pytest.raises(AccessError):
         message_remove(test_user0['token'], message0_id)
-    
--- src/message_send_test.py
+++ src/message_send_test.py
@@ -30,7 +30,7 @@ for i in range(chars):
     msg_char = "hello"
     message3 += msg_char
 
-@pytest.mark.skip(reason='function implementation not done yet')    
+@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_lessthan1000chars_existinguser():
     clear()
     test_user_0 = create_one_test_user()
@@ -54,15 +54,15 @@ def test_message_send_message_empty():
     with pytest.raises(error.InputError):
        assert message_send(test_user_0['token'], channel_name1, message_empty)
 
-@pytest.mark.skip(reason='function implementation not done yet')  
+@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_morethan1000chars():
     clear()
     test_user_0 = create_one_test_user()
     channel_name1 = channels_create(test_user_0['token'], "Main Channel", True)
     with pytest.raises(error.InputError):
        assert message_send(test_user_0['token'], channel_name1, message3)
-    
-@pytest.mark.skip(reason='function implementation not done yet')  
+
+@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_in_order():
     clear()
     test_user_0, test_user_1 = create_two_test_users()
@@ -73,10 +73,10 @@ def test_message_send_in_order():
     assert message_send(test_user_1['token'], channel_name1, message1) == {}
     #assert message_send(test_user_1['token'], channel_name1, message1) == 1
     assert message_send(test_user_1['token'], channel_name1, message1) == {}
-    #assert message_send(test_user_0['token'], channel_name1, message1) == 2 
+    #assert message_send(test_user_0['token'], channel_name1, message1) == 2
     assert message_send(test_user_0['token'], channel_name1, message1) == {}
 
-@pytest.mark.skip(reason='function implementation not done yet')  
+@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_usernotinchannel():
     clear()
     test_user_0 = create_one_test_user()
