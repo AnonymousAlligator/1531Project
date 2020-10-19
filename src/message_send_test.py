ea98641f60d9f9f45727678ab92c231d21afdbb8
Kevin Hu <kevin.hu1226@gmail.com>
2020-10-20 10:56:38 +1100

    Unskipped tests for message_send

--- src/message_send_test.py
+++ src/message_send_test.py
@@ -36,40 +36,30 @@ message_spaces += " "*100
 
 
 #Checking message ID's are sent in assigned in order
-@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_in_order():
     clear()
     test_user_0, test_user_1 = create_two_test_users()
     channel_name1 = channels_create(test_user_0['token'], "Main Channel", True)
     channel_join(test_user_1['token'],channel_name1)
-    #TODO: update in iter2
-    #assert message_send(test_user_1['token'], channel_name1, message1) == 0
-    assert message_send(test_user_1['token'], channel_name1, message1) == {}
-    #assert message_send(test_user_1['token'], channel_name1, message1) == 1
-    assert message_send(test_user_1['token'], channel_name1, message1) == {}
-    #assert message_send(test_user_0['token'], channel_name1, message1) == 2
-    assert message_send(test_user_0['token'], channel_name1, message1) == {}
+    assert message_send(test_user_1['token'], channel_name1, message1) == {'message_id': 0}
+    assert message_send(test_user_1['token'], channel_name1, message1) == {'message_id': 1}
+    assert message_send(test_user_0['token'], channel_name1, message1) == {'message_id': 2}
 
 #Successfully sending message less than 1000 characters
-@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_lessthan1000chars_existinguser():
     clear()
     test_user_0 = create_one_test_user()
     channel_name1 = channels_create(test_user_0['token'], "Main Channel", True)
-    #assert message_send(test_user_0['token'], 0, message1) == 0 #TODO: update in iter2
-    assert message_send(test_user_0['token'], channel_name1, message1) == {}
+    assert message_send(test_user_0['token'], channel_name1, message1) == {'message_id': 0}
 
 #Successfully sending message exactly 1000 characters
-@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_exactly1000chars():
     clear()
     test_user_0 = create_one_test_user()
     channel_name1 = channels_create(test_user_0['token'], "Main Channel", True)
-    #assert message_send(test_user_0['token'], 0, message2) == 1 #TODO: update in iter2
-    assert message_send(test_user_0['token'], channel_name1, message2) == {}
+    assert message_send(test_user_0['token'], channel_name1, message2) == {'message_id': 0}
 
 #Attempting to send an empty message
-@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_message_empty():
     clear()
     test_user_0 = create_one_test_user()
@@ -78,7 +68,6 @@ def test_message_send_message_empty():
        assert message_send(test_user_0['token'], channel_name1, message_empty)
 
 #Attempting to send a message with only spaces
-@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_message_spaces():
     clear()
     test_user_0 = create_one_test_user()
@@ -87,7 +76,6 @@ def test_message_send_message_spaces():
        assert message_send(test_user_0['token'], channel_name1, message_spaces)
 
 #Attempting to send a message with over 1000 characters
-@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_morethan1000chars():
     clear()
     test_user_0 = create_one_test_user()
@@ -97,12 +85,9 @@ def test_message_send_morethan1000chars():
 
 
 #Attempting to send a message when the user is not in the channel
-@pytest.mark.skip(reason='function implementation not done yet')
 def test_message_send_usernotinchannel():
     clear()
-    test_user_0 = create_one_test_user()
+    test_user_0, test_user_1 = create_two_test_users()
     channel_name1 = channels_create(test_user_0['token'], "Main Channel", True)
     with pytest.raises(error.AccessError):
-       assert message_send(test_user_0['token'], channel_name1, message1)
-
-
+       assert message_send(test_user_1['token'], channel_name1, message1)
