3941eee3741d2b0ce88885ef8163a724c307ce72
Kevin Hu <kevin.hu1226@gmail.com>
2020-10-20 15:14:05 +1100

    implemented message_send and remove wrappers

--- src/server.py
+++ src/server.py
@@ -31,5 +31,15 @@ def echo():
         'data': data
     })
 
+@APP.route("message/send", methods=['POST'])
+def http_message_send():
+    data = request.get_json()
+    return dumps(message.message_send(data['token'], data['channel_id'], data['message']))
+
+@APP.route("message/remove", methods=['DELETE'])
+def http_message_remove():
+    data = request.get_json()
+    return dumps(message.message_remove(data['token'], data['message_id']))
+
 if __name__ == "__main__":
     APP.run(port=0) # Do not edit this port
