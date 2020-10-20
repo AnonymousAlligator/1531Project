d46df999e4299b336e800bfded5e8b3f0e0870bb
Dylan Ngo <dylan.ngo09879@gmail.com>
2020-10-20 13:26:23 +1100

    Pylint issues

--- src/admin_userpermission_change_test.py
+++ src/admin_userpermission_change_test.py
@@ -2,7 +2,7 @@ import pytest
 import error
 from auth import auth_register
 from channels import channels_create
-from test_helpers import create_one_test_user, create_two_test_users, create_three_test_users
+from test_helpers import create_one_test_user, create_two_test_users
 from other import clear, admin_userpermission_change
 
 def test_admin_userpermission_change_successtoowner():
@@ -19,9 +19,9 @@ def test_admin_userpermission_change_successtodefault():
 
 def test_admin_userpermission_change_invalidtoken():
     clear()
-    user_0, user_1 = create_two_test_users()
+    user_0 = create_one_test_users()
     with pytest.raises(error.AccessError):
-        assert admin_userpermission_change('hello', user_1['u_id'], 1) == {}
+        assert admin_userpermission_change('hello', user_0['u_id'], 1) == {}
 
 def test_admin_userpermission_change_notflockrowner():
     clear()
@@ -31,7 +31,7 @@ def test_admin_userpermission_change_notflockrowner():
 
 def test_admin_userpermission_change_userdoesnotexist():
     clear()
-    user_0, user_1 = create_two_test_users()
+    user_0 = create_one_test_users()
     with pytest.raises(error.InputError):
         assert admin_userpermission_change(user_0['token'], 2, 1) == {}
 
