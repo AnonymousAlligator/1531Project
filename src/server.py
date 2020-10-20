b80447695531c25afe7506b01072b5de02663d83
Effie Hong <e.hong3648@gmail.com>
2020-10-20 20:43:21 +1100

    wrapper for user profile and set handle

--- src/server.py
+++ src/server.py
@@ -3,6 +3,8 @@ from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
 from error import InputError
+import user
+
 
 def defaultHandler(err):
     response = err.get_response()
@@ -26,10 +28,22 @@ APP.register_error_handler(Exception, defaultHandler)
 def echo():
     data = request.args.get('data')
     if data == 'echo':
-   	    raise InputError(description='Cannot echo "echo"')
+        raise InputError(description='Cannot echo "echo"')
     return dumps({
         'data': data
     })
+
+
+@APP.route("user/profile", methods=['GET'])
+def http_user_profile():
+    data = request.get_json()
+    return dumps(user.user_profile(data['token'], data['u_id']))
+
+@APP.route("user/profile/sethandle", methods = ['PUT']) 
+def http_user_profile_sethandle():
+    data = request.get_json()
+    return dumps(user.user_profile_sethandle(data['token'], data['handle_str']))
+
 
 if __name__ == "__main__":
     APP.run(port=0) # Do not edit this port
