b8efbabe224a0953fab46572cc223dd7b401e13a
Dylan Ngo <dylan.ngo09879@gmail.com>
2020-10-22 16:36:53 +1100

    Added test

--- http_tests/admin_userpermission_change_http_test.py
+++ http_tests/admin_userpermission_change_http_test.py
@@ -3,7 +3,6 @@ from url_fixture import url
 import pytest
 import requests
 
-@pytest.fixture
 def test_admin_userpermission_change_successtoowner(url):
     #clear data
     requests.delete(f'{url}/clear')
@@ -33,3 +32,60 @@ def test_admin_userpermission_change_successtoowner(url):
     payload = r.json()
 
     assert payload == {}
+
+def test_admin_userpermission_change_successtodefault():
+    #clear data
+    requests.delete(f'{url}/clear')
+    
+    #register test users
+    user0 = requests.post(f'{url}/auth/register', json={
+        "email" : "testemail0@email.com",
+        "password" : "valid_pw0",
+        "name_first" : "Hayden",
+        "name_last" : "Jacobs",
+    })
+    user0 = user0.json()
+
+    user1 = requests.post(f'{url}/auth/register', json={
+        "email" : "testemail1@email.com",
+        "password" : "valid_pw1",
+        "name_first" : "Jayden",
+        "name_last" : "Haycobs",
+    })
+    user1 = user1.json()
+
+    r = requests.post(f'{url}/admin/userpermission/change', json={
+        'token' : user0['token'],
+        'u_id' : user1['u_id'],
+        'permission_id' : 1,
+    })
+    r = requests.post(f'{url}/admin/userpermission/change', json={
+        'token' : user1['token'],
+        'u_id' : user0['u_id'],
+        'permission_id' : 2,
+    })
+    payload = r.json()
+
+    assert payload == {}
+
+def test_admin_userpermission_change_invalidtoken():
+    #clear data
+    requests.delete(f'{url}/clear')
+
+    #register test users
+    user0 = requests.post(f'{url}/auth/register', json={
+        "email" : "testemail0@email.com",
+        "password" : "valid_pw0",
+        "name_first" : "Hayden",
+        "name_last" : "Jacobs",
+    })
+    user0 = user0.json()
+
+    r = requests.post(f'{url}/admin/userpermission/change', json={
+        'token' : 'hello',
+        'u_id' : user0['u_id'],
+        'permission_id' : 1,
+    })
+    payload = r.json()
+
+    assert payload['code'] == 400
