cbe782dedcda0b98b5a6a2ecb55ed4aeb06b8b49
Dylan Ngo <dylan.ngo09879@gmail.com>
2020-10-22 17:19:08 +1100

    Added test

--- http_tests/channel_invite_http_test.py
+++ http_tests/channel_invite_http_test.py
@@ -53,9 +53,12 @@ def test_channel_invite_owner_pass(url, initialisation):
     user0, user1, _, channel0_id, _ = initialisation
     r = requests.post(f'{url}/channel/invite', json={
         'token' : user0['token'],
-        'channel_id' : channel_id0['channel_id'],
+        'channel_id' : channel0_id['channel_id'],
         'u_id' : user1['u_id']
     })
     payload = r.json()
     
     assert payload == {}
+
+def test_channel_invite_member_pass(url, initialisation):
+    user0, user1, user2, channel0_id, _ = initialisation
