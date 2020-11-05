4f6edb8f5e90f98452b555d682d3c985efaaa56e
Kevin Hu <kevin.hu1226@gmail.com>
2020-11-05 20:17:50 +1100

    Implemented message_sendlater and it's http wrapper

--- src/message.py
+++ src/message.py
@@ -1,6 +1,7 @@
 from other import data, check_token
 import error
 import datetime
+import threading
 
 def message_send(token, channel_id, message):
     # Check that the token is valid
@@ -153,3 +154,64 @@ def message_edit(token, message_id, message):
         if message_id == messages['message_id']:
             messages['message'] = message
             return {}
+
+def message_sendlater(token, channel_id, message, time_sent):
+    # Check that the token is valid
+    caller = check_token(token)
+
+    # Capture the current time asap
+    current_time = (datetime.datetime.now()).timestamp()
+
+    # Find the channel
+    target_channel = {}
+    for channel in data['channels']:
+        if channel_id == channel['id']:
+            target_channel = channel
+    # Input Error if the channel doesn't exist
+    if target_channel == {}:
+        #Input Error if the channel doesn't exist
+        raise error.InputError('Channel does not exist')
+
+    # Check to see if caller is part of that channel
+    is_member = False
+    for member in target_channel['all_members']:
+        if member['u_id'] == caller['u_id']:
+            is_member = True
+    # Access Error if the person inviting is not within the server
+    if not is_member:
+        raise error.AccessError('You are not part of the channel you want to send messages to')
+
+    # Check the message length for issues
+    if len(message) > 1000 or len(message) < 1 or len(message.strip()) < 1:
+        raise error.InputError('The message you are sending is over 1000 characters')
+
+    # Check the time is not from before current
+    if (time_sent - current_time) < 0:
+        raise error.InputError('Trying to send message in the past')
+
+    delay = time_sent - current_time
+    threading.Timer(delay, send_message, kwargs={'caller':caller, 'message':message, 'target_channel':target_channel, 'channel_id':channel_id}).start()
+
+def send_message(caller, message, target_channel, channel_id):
+    # message gets added to the channel's message key
+    if len(data['messages']) == 0:
+        message_id = 0
+    else:
+        message_id = data['messages'][0]['message_id'] + 1
+    time_created = round((datetime.datetime.now()).timestamp())
+    channel_message = {'message_id': message_id,
+                    'u_id': caller['u_id'],
+                    'message': message,
+                    'time_created': time_created}
+    target_channel['messages'].insert(0, channel_message)
+
+    # adding data to messages for easier searching
+    message_data = {'message_id': message_id,
+                    'u_id': caller['u_id'],
+                    'message': message,
+                    'time_created': time_created,
+                    'channel_id' : channel_id,}
+    data['messages'].insert(0, message_data)
+    return {
+        'message_id': message_id
+    }
