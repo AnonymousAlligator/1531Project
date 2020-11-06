57b6c77a9024a9f57d601044df49ab81522bf229
Kevin Hu <kevin.hu1226@gmail.com>
2020-11-06 16:52:42 +1100

    wrote http and regular tests for standup_active

--- src/standup.py
+++ src/standup.py
@@ -5,7 +5,7 @@ import time
 
 def standup_active(token, channel_id):
     # Check that the token is valid
-    _ = check_token(token)
+    caller = check_token(token)
 
     # Find the channel
     target_channel = {}
@@ -16,6 +16,14 @@ def standup_active(token, channel_id):
     if target_channel == {}:
         #Input Error if the channel doesn't exist
         raise error.InputError('Channel does not exist')
+    
+    # Check to see if caller is part of that channel
+    is_member = False
+    for member in target_channel['all_members']:
+        if member['u_id'] == caller['u_id']:
+            is_member = True
+    if not is_member:
+        raise error.AccessError('You are not part of the channel')
 
     if target_channel['standup']['is_standup']:
         return {'is_active': True, 'time_finish': target_channel['standup']['time_finish']}
@@ -44,7 +52,7 @@ def standup_start(token, channel_id, length):
     target_channel['standup']['time_finish'] = end_time
     
     #Begins thread that changes values in target channel to indicate standup finished when timer is up
-    t = threading.Timer(length, end_standup(target_channel))
+    t = threading.Timer(length, end_standup, kwargs={'target_channel':target_channel})
     t.start()
 
     return {'time_finish': end_time}
