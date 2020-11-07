fea081c747b3c707e605232c073f6d9ffe52bc46
Dylan Ngo <dylan.ngo09879@gmail.com>
2020-11-07 14:00:53 +1100

    Added routes for server for functions message_pin and message_unpin as well as adding an message_unpin function

--- src/server.py
+++ src/server.py
@@ -126,6 +126,16 @@ def http_message_edit():
     data = request.json
     return dumps(message.message_edit(data['token'], int(data['message_id']), data['message']))
 
+@APP.route("/message/pin", methods=['POST'])
+def http_message_pin():
+    data = request.json
+    return dumps(message.message_pin(data['token'], int(data['message_id'])))
+
+@APP.route("/message/unpin", methods=['POST'])
+def http_message_unpin():
+    data = request.json
+    return dumps(message.message_unpin(data['token'], int(data['message_id'])))
+
 @APP.route("/user/profile", methods=['GET'])
 def http_user_profile():
     token = request.args.get('token')
