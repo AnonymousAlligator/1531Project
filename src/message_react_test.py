baf45f7abbe157c09795ae4b76b3c51202b8b7fc
Kevin Hu <kevin.hu1226@gmail.com>
2020-11-08 22:30:38 +1100

    removed duplicate test

--- src/message_react_test.py
+++ src/message_react_test.py
@@ -36,21 +36,6 @@ def test_message_react_invalid_reactid():
     with pytest.raises(InputError):
         assert message_react(test_user0['token'], message0_id['message_id'], 5)
 
-# Check that fails for invalid react_id
-def test_message_react_invalid_token(): 
-    clear()
-    test_user0 = create_one_test_user()
-
-    # test_user0 creates 1 public channel
-    channel_id = channels_create(test_user0['token'], "Public Channel", True)
-        
-    # test_user0 sends 1 message to public channel
-    message0 = "inital message"
-    message0_id = message_send(test_user0['token'], channel_id['channel_id'], message0)
-
-    with pytest.raises(AccessError):
-        assert message_react('hello', message0_id['message_id'], 1)
-
 # Check that fails for invalid message_id
 def test_message_react_invalid_msg_id(): 
     clear()
@@ -61,7 +46,7 @@ def test_message_react_invalid_msg_id():
         
     # test_user0 sends 1 message to public channel
     message0 = "inital message"
-    message0_id = message_send(test_user0['token'], channel_id['channel_id'], message0)
+    message_send(test_user0['token'], channel_id['channel_id'], message0)
 
     with pytest.raises(InputError):
         assert message_react(test_user0['token'], 2, 1)
@@ -80,4 +65,3 @@ def test_message_react_invalid_token():
 
     with pytest.raises(AccessError):
         assert message_react('hello', message0_id['message_id'], 1)
-
