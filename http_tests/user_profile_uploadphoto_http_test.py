ac982cd042eef99618e75f6cbdc7866d4f0cae32
Effie Hong <e.hong3648@gmail.com>
2020-11-08 17:13:29 +1100

    added some http tests

--- http_tests/user_profile_uploadphoto_http_test.py
+++ http_tests/user_profile_uploadphoto_http_test.py
@@ -1,4 +1,5 @@
 from url_fixture import url
+from flask import Flask, request, send_from_directory
 import pytest
 import requests
 
@@ -16,11 +17,32 @@ def initialisation(url):
     ross = user1.json()
     return ross
 
+
 def test_user_profile_photo_success(url, initialisation):
     user0 = initialisation
     r = requests.post(f'{url}/user/profile/uploadphoto', json={
         'token' : user0['token'],
+        'img_url' : 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Teemo_0.jpg',
+        'x_start' : '600',
+        'y_start' : '100',
+        'x_end' :'1000',
+        'y_end' : '400',
+    })
+    #payload = send_from_directory('static', f'{user0["token"]}.jpeg')
+    payload = r.json()
+    assert payload == {}
+    
+
+
+def test_user_profile_photo_width_too_large(url, initialisation):
+    user0 = initialisation
+    r = requests.post(f'{url}/user/profile/uploadphoto', json={
+        'token' : user0['token'],
+        'img_url' : 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Teemo_0.jpg',
+        'x_start' : '0',
+        'y_start' : '0',
+        'x_end' :'1220',
+        'y_end' : '717',
     })
     payload = r.json()
-    
-    assert payload == {}
+    assert payload['code'] == 400
