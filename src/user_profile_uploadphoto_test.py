ddd4747fe6b6c0968e3c0aecf12e521db5021188
Effie Hong <e.hong3648@gmail.com>
2020-11-08 14:37:56 +1100

    added a few http tests

--- src/user_profile_uploadphoto_test.py
+++ src/user_profile_uploadphoto_test.py
@@ -5,6 +5,11 @@ from test_helpers import create_one_test_user
 import pytest
 
 
+def test_user_profile_photo_success():
+    clear()
+    test_user0 = create_one_test_user()
+    assert user_profile_uploadphoto(test_user0['token'], 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Teemo_0.jpg', 600, 100, 1000, 400) == {}
+
 def test_user_profile_photo_width_too_large():
     clear()
     test_user0 = create_one_test_user()
@@ -22,8 +27,3 @@ def test_user_profile_photo_not_jpeg():
     test_user0 = create_one_test_user()
     with pytest.raises(InputError):
         assert user_profile_uploadphoto(test_user0['token'], 'https://static.wikia.nocookie.net/leagueoflegends/images/d/d6/Teemo_Render.png/revision/latest?cb=20190112183654', 0, 0, 200, 200) == {}
-
-def test_user_profile_photo_success():
-    clear()
-    test_user0 = create_one_test_user()
-    assert user_profile_uploadphoto(test_user0['token'], 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Teemo_0.jpg', 600, 100, 1000, 400) == {}
