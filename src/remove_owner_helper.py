1efe131ed08f5a396721ecb47bdd803af085a1d6
Kevin Hu <kevin.hu1226@gmail.com>
2020-11-13 16:23:37 +1100

    removed unused functions

--- src/remove_owner_helper.py
+++ src/remove_owner_helper.py
@@ -8,18 +8,3 @@ def remove_helper_func(channel_id, removed_person):
                 if removed_person['u_id'] == owner['u_id']:
                     channel['owner_members'].remove(owner)
                     return {}
-
-
-def find_with_uid(u_id):
-    for user in data['users']:
-        if u_id == user['u_id']:
-            return(user)
-    # If we are here then the token was invalid
-    raise error.AccessError('The user ID does not exist')
-
-def check_member_of_channel(removed_person):
-    for channel in data['channels']:
-        for member in channel['all_members']:
-            if removed_person['u_id'] == member['u_id']:
-                return(member)
-        raise error.InputError('The person you are trying to add is not a member of this channel')
